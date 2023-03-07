2fa79f2daedda681a9f1f8d3a45019078c5087ab
Ryan Engel <Rcengel@comcast.net>
2023-03-06 18:59:48 -0800

    pages created and else if conditionals done

---
+++ index.js
@@ -0,0 +1,38 @@
+let firstCard = 3
+let secondCard = 9
+
+let sum = firstCard + secondCard + 10
+console.log(sum)
+
+if (sum <= 20) {
+    console.log("Do you want to draw a new card?")
+}
+else if (sum === 21) {
+    console.log("Congratulations!")
+}
+else {
+    console.log("You're out of the game!")
+}
+//if else conditionals 
+
+//if (sum < 21) {
+    //console.log("Do you want to draw a new card?")
+//}
+//else if (sum === 21) {
+   // console.log("Congratulations!")
+//}
+//else {
+    //console.log("Better luck next time")
+//}
+
+//let age = 145
+
+//if (age < 100) {
+    //console.log("Not eligible!")
+//}
+//else if (age === 100) {
+    //console.log("Here is your birthday card from the King!")
+//}
+//else {
+    //console.log("Not eligible, you have already gotten one!")
+//}
